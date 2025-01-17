76bcc5ac46abde205ed6622bbe6ae366a1831c8e
Anubhav-Sah <anupsah2005@gmail.comgit>
2025-01-17 23:15:27 +0530

    conversion of data type

---
+++ javascript/conversionOperation.js
@@ -0,0 +1,31 @@
+let score = true
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+
+console.log(typeof valueInNumber);
+
+
+console.log(valueInNumber);
+
+//            For Number
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+
+let isloggedIn = 5
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true  ;  0 => false
+//"" => false ; "Anubhav" =>true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
