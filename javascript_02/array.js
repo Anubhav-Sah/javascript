362b6e05152d52020a4239bd62baf4358b3fe356
Anubhav-Sah <anupsah2005@gmail.comgit>
2025-01-21 23:00:32 +0530

    array

---
+++ javascript_02/array.js
@@ -0,0 +1,26 @@
+//aray
+
+const myArray = [0,1,2,3,4,5]
+
+console.log(myArray[0]);
+
+const myArray2 = new Array(1,2,3)
+
+console.log(myArray2);
+
+
+//Arary Methods
+
+myArray.push(6)
+myArray.push(7)
+myArray.pop()
+myArray.unshift(9)
+myArray.shift()
+
+
+console.log(myArray);
+
+console.log(myArray.includes(9));
+
+console.log(myArray.indexOf(9));
+
